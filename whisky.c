5108efaec22eee0129d1f7a39c0f913da797b40c
xenku <xenku.code@gmail.com>
2026-07-26 08:57:22 +0530

    video to ascii converter

--- whisky.c
+++ whisky.c
@@ -35,14 +35,12 @@ int main(int argc, char *argv[])
     char command2[500];
     sprintf(command1, "ffmpeg -i %s -vf \"scale=%d:%d,fps=%s\" frames/frame_%%06d.png", argv[1],w.ws_col,w.ws_row - 1,argv[2]);
     sprintf(command2, "ffmpeg -i %s -vn -ar 44100 -ac 2 audio.mp3", argv[1]);
-    
-    system(command1);
-    system(command2);
 
     if(system(command1) != 0){
         printf("Failed to extract frames. FFmpeg might not be installed.\n");
         return 1;
     }
+    system(command2);
     
     ma_engine engine;
     if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
